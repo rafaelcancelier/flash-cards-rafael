389f7ba412c802adc77bc6f5e7156461cec53e51
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2024-10-18 12:27:54 -0300

    18/10

---
+++ pergunts.js
@@ -0,0 +1,29 @@
+criaCartao(
+'Esportes',
+'Quantos gols Cristiano Ronaldo tem ?',
+'906'
+)
+
+criaCartao(
+    'Historia',
+    'qual e o pais mais antio do mundo?',
+    'Egito'
+    )
+
+    criaCartao(
+        'Categoria',
+        'Pergunta?',
+        'Resposta'
+        )
+
+        criaCartao(
+            'Categoria',
+            'Pergunta?',
+            'Resposta'
+            )
+
+            criaCartao(
+                'Categoria',
+                'Pergunta?',
+                'Resposta'
+                )
