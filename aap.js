6551506c0086153d7049b6bbdc4ec0b73322e863
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2024-10-25 08:55:27 -0300

    25/10

--- aap.js
+++ aap.js
@@ -1,3 +1,18 @@
 function criaCartao ( Categoria, pergunta, resposta){
-    console.log(Categoria, pergunta, resposta);
+   let container = document.getElementById( 'container');
+   let catao = document.createElement('article');
+   criaCartao.clasName = 'cartao';
+
+   cartao.innerHTML = `
+     <div class="cartao__conteudo">
+                 <h3>${categoria}</h3>
+                 <div class="cartao__conteudo__pergunta">
+                        <p>$[pergunta]</p>
+                </div>
+                <div class="cartao__conteudo__resposta">
+                        <p>${resposta}</p>
+                    </div>
+     </div> 
+   `
+   container.appendChild(catao);
 }
