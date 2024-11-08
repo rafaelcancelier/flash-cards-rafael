d8f7eb3f2d8dff4f65dcf7ee8c960160903b18ec
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2024-11-08 12:20:26 -0300

    08/11

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
-'Esportes',
-'Quantos gols Cristiano Ronaldo tem ?',
-'907'
+    'Esportes',
+    'Quantos gols Cristiano Ronaldo tem ?',
+    '907'
 )
 
 criaCartao(
     'Historia',
     'qual e o pais mais antio do mundo?',
     'Egito'
-    )
+)
 
-    criaCartao(
-        'Conhecimento Geral',
-        'Da onde veio a inveção do chueiro eletrico ?',
-        'Brasil',
-        )
+criaCartao(
+    'Conhecimento Geral',
+    'Da onde veio a inveção do chueiro eletrico ?',
+    'Brasil'
+)
 
-        criaCartao(
-            'Esportes',
-            'Quantos gols Cristiano Ronaldo tem na champions league ?',
-            '141',
-            )
-            
+criaCartao(
+    'Esportes',
+    'Quantos gols Cristiano Ronaldo tem na champions league ?',
+    '141'
+)
+
