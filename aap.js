0a93ad32583f81624acc8170cb18348e3a153b9c
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2024-11-08 12:18:29 -0300

    08/11

--- aap.js
+++ aap.js
@@ -1,28 +1,29 @@
-function criaCartao(Categoria, pergunta, resposta) {
+function criaCartao(categoria, pergunta, resposta) {
   let container = document.getElementById('container');
-  let catao = document.createElement('article');
-  cartao.clasName = 'cartao';
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
 
   cartao.innerHTML = `
-              <div class="cartao__conteudo">
-                   <h3>${categoria}</h3>
-                   <div class="cartao__conteudo__pergunta">
-                        <p>${pergunta}</p>
-                </div>
-                <div class="cartao__conteudo__resposta">
-                        <p>${resposta}</p>
-                 </div>
-              </div> 
-   `
+  <div class="cartao__conteudo">
+  <h3>${categoria}</h3>
+  <div class="cartao__conteudo__pergunta">
+  <p>${pergunta}</p>
+  </div>
+  <div class="cartao__conteudo__resposta">
+  <p>${resposta}</p>
+  </div>
+  </div>
+  `
 
-  let repostaEstaVisivel = false;
+  let respostaEstaVisivel = false;
 
   function viraCartao() {
-    repostaEstaVisivel = !repostaEstaVisivel;
-    cartao.classlist.toggle('active', repostaEstaVisivel);
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel);
   }
+  cartao.addEventListener('click', viraCartao);
 
-  cartao.addEventListener('click', viraCartao)
 
-  container.appendChild(catao);
+  container.appendChild(cartao);
+
 }
